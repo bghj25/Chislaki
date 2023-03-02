1fee07f2aae27a345357adca49ca5a0edeb9ef14
bghj25 <bghj25@ya.ru>
2023-03-02 12:55:58 +0300

    1st, 2nd derivative

--- lab2.py
+++ lab2.py
@@ -7,7 +7,7 @@ h = (b - a)/n
 
 x_arr = np.linspace(a - 2 * (b - a) / (n - 1), b + 2 * (b - a) / (n - 1), n + 4)
 #print(x_arr)
-y_arr = x_arr ** 2 / 2
+y_arr = x_arr ** 3
 x_arr_to_plot = np.delete(x_arr, [0, 1, len(x_arr) - 1, len(x_arr) - 2])
 y_arr_to_plot = np.delete(y_arr, [0, 1, len(y_arr) - 1, len(y_arr) - 2])
 
@@ -38,8 +38,11 @@ fig.suptitle('Вторая производная, второй порядок �
 plt.plot(x_arr_to_plot, y_arr_to_plot)
 plt.plot(x_arr_to_plot, y_arr_derivative)
 
+
 y_arr_derivative = []
+print(y_arr_to_plot)
 for i in range(2, n + 2):
+
     y_arr_derivative.append((-y_arr[i + 2] + 16 * y_arr[i + 1] * y_arr[i] - 30 * y_arr[i] +
                              16 * y_arr[i - 1] - y_arr[i - 2])/(h ** 4))
 
